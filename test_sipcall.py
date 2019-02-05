b8f40afba5a21f99bb5f2b177176a7dd056f79c6
grafst <grafst>
2019-02-06 00:43:27 +0100

    refactor tests and work on sipcallgetter:login

--- test_sipcall.py
+++ test_sipcall.py
@@ -18,33 +18,29 @@ class TestSipCall(unittest.TestCase):
     def test_sipcallgetter_has_login_function(self):
         self.assertTrue(callable(sipcall.SipCallGetter._login))
 
+    """ def test_cannot_login_if_url_not_reachable(self):
+         httpretty.register_uri(httpretty.GET, self.sipCallGetter.login_url, body="", status=404)
+         self.assertRaises(requests.RequestException, self.sipCallGetter._login)
+    """
 
-""" def test_cannot_login_if_url_not_reachable(self):
-     httpretty.register_uri(httpretty.GET, self.sipCallGetter.login_url, body="", status=404)
-     self.assertRaises(requests.RequestException, self.sipCallGetter._login)
-"""
+    def test_if_login_logs_error_on_http400(self):
+        httpretty.register_uri(httpretty.GET, self.sipCallGetter.login_url, body="", status=404)
+        with self.assertLogs(self.logger, level='ERROR'):
+            self.sipCallGetter._login()
 
+    def test_sucessful_login_returns_a_session_object(self):
+        # todo: make sure its sucessful
+        sipCallGetter = sipcall.SipCallGetter()
+        self.assertIsInstance(sipCallGetter._login(), requests.Session)
 
-def test_if_login_logs_error_on_http400(self):
-    httpretty.register_uri(httpretty.GET, self.sipCallGetter.login_url, body="", status=404)
-    with self.assertLogs(self.logger, level='ERROR'):
-        self.sipCallGetter._login()
-
-
-def test_sucessful_login_returns_a_session_object(self):
-    # todo: make sure its sucessful
-    sipCallGetter = sipcall.SipCallGetter()
-    self.assertIsInstance(sipCallGetter._login(), requests.Session)
-
-
-def test_configfile_sipcall_section(self):
-    self.assertTrue(bb.config.has_section("sipcall"))
-    missing_subtests = (
-        # A tuple of (option_name, subtest_description)
-        ('username', 'Missing the first_name field'),
-        ('login_url', 'Missing the last_name field'),
-        ('password', 'Missing the address field'),
-    )
-    for option_name, subtest_description in missing_subtests:
-        with self.subTest(subtest_description):
-            self.assertTrue(bb.config.has_option("sipcall", option_name))
+    def test_configfile_sipcall_section(self):
+        self.assertTrue(bb.config.has_section("sipcall"))
+        missing_subtests = (
+            # A tuple of (option_name, subtest_description)
+            ('username', 'Missing the first_name field'),
+            ('login_url', 'Missing the last_name field'),
+            ('password', 'Missing the address field'),
+        )
+        for option_name, subtest_description in missing_subtests:
+            with self.subTest(subtest_description):
+                self.assertTrue(bb.config.has_option("sipcall", option_name))
